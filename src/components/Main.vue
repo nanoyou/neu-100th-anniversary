1beccfb802ee5238d0e7f12f3b9eed5baefe8440
PermissionDog <455662503@qq.com>
2022-11-05 23:26:06 +0800

    114

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,15 +2,33 @@
 
 </script>
 <template>
-    <div id="main">
-        
+    <div class="main">
+        <h1>东北大学 百年校庆</h1>
+        <h2>百年沧桑，欣逢盛世。满天桃李，共庆华诞。</h2>
     </div>
 </template>
 <style lang="less" scoped>
-div {
+.main {
     background-image: url(img/neu_nanhu_night_scene.png);
     background-size: cover;
 
     height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+}
+
+h1, h2 {
+    color: white;
+}
+h1 {
+    font-size: 100px;
+    text-shadow: 10px 10px 10px #3f3f3f;
+}    
+h2 {
+    font-size: 30px;
 }
 </style>
