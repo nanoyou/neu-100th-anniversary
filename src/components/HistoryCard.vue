7def93a03965947ac733306d560730e2175dacaa
PermissionDog <455662503@qq.com>
2022-11-07 16:14:50 +0800

    卡片

---
+++ src/components/HistoryCard.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    year: number,
+    title: string,
+    body: string,
+    icon?: string,
+}>()
+
+</script>
+<template>
+    <div class="card">
+        <h2 class="title">{{props.title}}</h2>
+
+        <div class="body">
+            <img class="body-img" :src="props.icon" v-if="props.icon" />
+            <p class="body-description">{{props.body}}</p>
+        </div>
+        <div class="ribbon">
+            {{props.year}}
+        </div>
+    </div>
+</template>
+<style lang="less" scoped>
+h2 {
+    margin: 0;
+}
+.card {
+    position: relative;
+    background-color: #FFF;
+    padding: 50px;
+    border-radius: 10px;
+    box-shadow: 5px 5px 8px #555;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 30vw;
+}
+.ribbon {
+    position: absolute;
+
+    top: 26px;
+    left: -38px;
+
+    padding: 8px 65px 8px 65px;
+    rotate: -45deg;
+    
+    color: white;
+    font-weight: bold;
+
+    background-image: url(/img/ribbon.svg);
+    background-repeat: no-repeat;
+    background-position: center;
+}
+.title {
+    font-size: 40px;
+    margin-bottom: 20px;
+}
+.body {
+    display: flex;
+    align-items: center;
+
+    &-img {
+        width: 100px;
+        margin-right: 50px;
+    }
+
+    &-description {
+        text-indent: 2em;
+        font-size: 22px;
+    }
+}
+</style>
--- src/view/History.vue
+++ src/view/History.vue
@@ -1,8 +1,30 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
+import HistoryCard from '../components/HistoryCard.vue'
 
+const cards = reactive([
+    {
+        year: 1922,
+        title: '学府筹办',
+        body: '1922年春，张作霖委派时任奉天省长兼财政厅长王永江筹办东北大学。同年，成立东北大学筹备委员会，原国立沈阳高等师范学校改办为东北大学理工科，原文学专门学校（原奉天法政学堂）改办为东北大学文法科。校长由王永江兼任，学生480余人。文法科设中国文学系、英文学系、俄文学系、法律学系、政治学系；理工科设数学系、物理学系、化学系、土木工学系，机械学系，并成立东北大学筹委会。',
+        icon: undefined,
 
-const currentCard = ref()
+        detailYear: '1922年',
+        detailMonth: '春季',
+
+        enabled: false,
+    }, {
+        year: 1923,
+        title: '东大之印',
+        body: '1923年4月19日，奉天省公署颁发“东北大学之印”，4月26日正式启用，东北大学宣告成立，时任奉天省省长王永江为首任校长。',
+        icon: '/img/neu_stamp.png',
+
+        detailYear: '1923年',
+        detailMonth: '4月',
+
+        enabled: true,
+    }
+])
 </script>
 <template>
     <div id="history">
@@ -10,7 +32,7 @@ const currentCard = ref()
             轴
         </aside>
         <main>
-            <component :is="currentCard" />
+            <HistoryCard v-for="{year, title, body, icon, enabled} in cards" v-show="enabled" :year="year" :title="title" :body="body" :icon="icon" />
         </main>
     </div>
 </template>
@@ -22,5 +44,8 @@ const currentCard = ref()
     display: flex;
     align-items: center;
     justify-content: center;
+}
+aside {
+    width: 20vw;
 }
 </style>
